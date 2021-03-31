716b90d54fd2455b2a15674698329d1cf0af20d1
antalpeti <antalpeti@users.noreply.github.com>
2021-03-31 23:31:26 +0200

    Add dependencies and read file content.

--- src/main/java/hu/nn/controller/ImportController.java
+++ src/main/java/hu/nn/controller/ImportController.java
@@ -1,10 +1,16 @@
 package hu.nn.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -13,6 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.google.common.base.Splitter;
+import com.opencsv.CSVReader;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -49,6 +58,9 @@ public class ImportController {
         try {
             path = Paths.get(IMPORT_DIR + originalFileName);
             Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+            processFileWithSplitter(Files.newInputStream(path));
+            processFileWithCSVReader(Files.newInputStream(path));
             log.info("Successfull import. originalFileName: {}", originalFileName);
             attributes.addFlashAttribute("success", "Import successfull: " + originalFileName);
         } catch (IOException e) {
@@ -57,6 +69,30 @@ public class ImportController {
         }
 
         return "redirect:/";
+    }
+
+    private void processFileWithSplitter(InputStream inputStream) {
+        log.info("processFileWithSplitter called.");
+        Scanner sc = new Scanner(inputStream, StandardCharsets.UTF_8.name());
+        String separator = "|";
+        while (sc.hasNext()) {
+            String nextLine = sc.nextLine();
+            log.info("nextLine: {}", nextLine);
+            List<String> splitToList = Splitter.on(separator).splitToList(nextLine);
+            log.info("splitToList: {}", splitToList);
+        }
+    }
+
+    private void processFileWithCSVReader(InputStream inputStream) {
+        log.info("processFileWithCSVReader called.");
+        try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8.name()))) {
+            List<String[]> lines = csvReader.readAll();
+            for (String[] line : lines) {
+                log.info("CSV line: {}", Arrays.toString(line));
+            }
+        } catch (Exception e) {
+            log.error("Error during csv parsing: {}", e);
+        }
     }
 
 }
