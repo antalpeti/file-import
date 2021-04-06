0317b8cf72c26390c57c91b5e37d5fc12b07c7ae
antalpeti <antalpeti@users.noreply.github.com>
2021-04-06 17:57:22 +0200

    Fix currency detection.

--- src/main/java/hu/nn/constant/SurValuesConstant.java
+++ src/main/java/hu/nn/constant/SurValuesConstant.java
@@ -5,8 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class SurValuesConstant {
 
-    public static final String COMPANY_EUR = "1";
-    public static final String COMPANY_HUF = "2";
+    public static final String COMPANY_EUR = "2";
+    public static final String COMPANY_HUF = "1";
 
     public static final String CURRENCY_EUR = "EUR";
     public static final String CURRENCY_HUF = "HUF";
