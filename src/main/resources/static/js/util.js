cd0c56a0649993596df0caff33024ce9656ea6ae
antalpeti <antalpeti@users.noreply.github.com>
2021-04-08 21:19:52 +0200

    Rename function. Synchronize textareas vertical and horizontal scrolling bars.

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -1,20 +1,22 @@
 const UTIL = {
-  scroll_two_textarea: function(id1, id2) {
-    const ta1 = document.getElementById(id1);
-    if(ta1 == null) {
-      return console.warn("[tln.js] Couldn't find textarea of id '"+id1+"'");
+  synchronize_scrolling_bars: function(id1, id2) {
+    const s1 = document.getElementById(id1);
+    if(s1 == null) {
+      return console.warn("[tln.js] Couldn't find element of id '"+id1+"'");
     }
-    const ta2 = document.getElementById(id2);
-    if(ta2 == null) {
-      return console.warn("[tln.js] Couldn't find textarea of id '"+id2+"'");
+    const s2 = document.getElementById(id2);
+    if(s2 == null) {
+      return console.warn("[tln.js] Couldn't find element of id '"+id2+"'");
     }
     function select_scroll_1(e) {
-        ta2.scrollTop = ta1.scrollTop;
+        s2.scrollTop = s1.scrollTop;
+        s2.scrollLeft = s1.scrollLeft;
     }
     function select_scroll_2(e) {
-        ta1.scrollTop = ta2.scrollTop;
+        s1.scrollTop = s2.scrollTop;
+        s1.scrollLeft = s2.scrollLeft;
     }
-    ta1.addEventListener('scroll', select_scroll_1, false);
-    ta2.addEventListener('scroll', select_scroll_2, false);
+    s1.addEventListener('scroll', select_scroll_1, false);
+    s2.addEventListener('scroll', select_scroll_2, false);
   }
 }
