059ab05b2b2da4cd31cab3709aa1f6e51ced3e32
antalpeti <antalpeti@users.noreply.github.com>
2021-04-08 21:36:52 +0200

    Put vertical and horizontal scrolling bar synchronization into different functions. Use only vertical scrolling bar synchronization.

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -1,5 +1,5 @@
 const UTIL = {
-  synchronize_scrolling_bars: function(id1, id2) {
+  synchronize_vertical_scrolling_bars: function(id1, id2) {
     const s1 = document.getElementById(id1);
     if(s1 == null) {
       return console.warn("[tln.js] Couldn't find element of id '"+id1+"'");
@@ -10,10 +10,26 @@ const UTIL = {
     }
     function select_scroll_1(e) {
         s2.scrollTop = s1.scrollTop;
-        s2.scrollLeft = s1.scrollLeft;
     }
     function select_scroll_2(e) {
         s1.scrollTop = s2.scrollTop;
+    }
+    s1.addEventListener('scroll', select_scroll_1, false);
+    s2.addEventListener('scroll', select_scroll_2, false);
+  },
+  synchronize_horizontal_scrolling_bars: function(id1, id2) {
+    const s1 = document.getElementById(id1);
+    if(s1 == null) {
+      return console.warn("[tln.js] Couldn't find element of id '"+id1+"'");
+    }
+    const s2 = document.getElementById(id2);
+    if(s2 == null) {
+      return console.warn("[tln.js] Couldn't find element of id '"+id2+"'");
+    }
+    function select_scroll_1(e) {
+        s2.scrollLeft = s1.scrollLeft;
+    }
+    function select_scroll_2(e) {
         s1.scrollLeft = s2.scrollLeft;
     }
     s1.addEventListener('scroll', select_scroll_1, false);
